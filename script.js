22724e1b5a0e104505141d0a41008a4d7fb989df
ydovbak <yulia.dovbak@gmail.com>
2020-11-14 00:29:18 +0000

    Finished daily tabs design

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 "use strict";
 
-window.onload = function () {
+const $ = (selector) => document.querySelector(selector);
 
+window.onload = () => {
+    $('.day-tab').addEventListener('click', (event) => {
+        console.log('called', event);
+        // $('#day-of-week').textContent = 'Friday';
+    });
 };
